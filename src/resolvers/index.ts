b8c6d21267d4d097882a75c1d8419ed6b7d5ccb8
bdCalling <bdcalling@bdCallings-Mac-mini.local>
2025-03-15 16:12:53 +0600

    first commit

---
+++ src/resolvers/index.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from "@prisma/client";
+
+
+
+const prisma = new PrismaClient()
+
+interface userInfo {
+    name: string,
+    email: string,
+    password: string
+}
+
+
+export const resolvers = {
+    Query: {
+        me:{
+            
+        }
+  
+    },
+    Mutation:{
+        signup:async(parent:any,args:userInfo,context:any)=>{
+   
+            return await prisma.user.create({
+                data:args
+            })
+
+        }
+    }
+  };
