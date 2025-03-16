980ec26b8e45d5e58bc1046a923cc686b4196da7
abdulAlim <alimsujon12@gmail.com>
2025-03-17 01:02:01 +0600

    user login and signup

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,32 +1,53 @@
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import { extendSchemaImpl } from "graphql/utilities/extendSchema";
+import jwt from "jsonwebtoken";
 
-
-
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 interface userInfo {
-    name: string,
-    email: string,
-    password: string
+  name: string;
+  email: string;
+  password: string;
 }
 
-
 export const resolvers = {
-    Query: {
-        users:async (parent:any,args:any,context:any)=>{
-
-            return await prisma.user.findMany()
-            
-        }
-  
+  Query: {
+    users: async (parent: any, args: any, context: any) => {
+      return await prisma.user.findMany();
     },
-    Mutation:{
-        signup:async(parent:any,args:userInfo,context:any)=>{
-   
-            return await prisma.user.create({
-                data:args
-            })
+  },
+  Mutation: {
+    signup: async (parent: any, args: userInfo, context: any) => {
+      const hashPassword = await bcrypt.hash(args.password, 12);
 
-        }
-    }
-  };
+      const newUser = await prisma.user.create({
+        data: { name: args.name, email: args.email, password: hashPassword },
+      });
+
+      const token = jwt.sign({ userId: newUser.id }, "fajf", {
+        expiresIn: "1d",
+      });
+    },
+
+    signIn: async (parent: any, args: any, contex: any) => {
+      const user = await prisma.user.findFirst({
+        where: {
+          email: args.email,
+        },
+      });
+      if (!user) {
+        return { token: null };
+      }
+
+      const correctPass = await bcrypt.compare(args.password, user.password);
+      if (!correctPass) {
+        return { token: null };
+      }
+
+      const token = jwt.sign({ userId: user.id }, "fajf", {
+        expiresIn: "1d",
+      });
+    },
+  },
+};
--- src/schema.ts
+++ src/schema.ts
@@ -1,38 +1,32 @@
-
 export const typeDefs = `#graphql
 
 type Query {
-
-me: User
-users: [User]
-posts: [Post]
-
+  me: User
+  users: [User]
+  posts: [Post]
 }
 
 type Mutation {
-
- signup(name:String!,email: String!,password:String!):User
-
-
+  signup(name: String!, email: String!, password: String!): AuthPayload
+  signIn(email:String! ,password: String!): AuthPayload
 }
 
-
 type Post {
-id: ID!
-title: String!
-content: String!
-author: User 
-createdAt: String!
-published: Boolean!
+  id: ID!
+  title: String!
+  content: String!
+  author: User
+  createdAt: String!
+  published: Boolean!
 }
 
 type User {
   id: ID!
   name: String!
-  email: String! 
+  email: String!
+  password: String
   createdAt: String!
   posts: [Post]
-
 }
 
 type Profile {
@@ -40,7 +34,11 @@ type Profile {
   bio: String!
   createdAt: String!
   user: User!
+}
 
+# New type to represent the auth payload
+type AuthPayload {
+  token: String
 }
 
 `;
